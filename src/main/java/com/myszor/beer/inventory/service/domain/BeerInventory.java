907d0d2db8b428d62903649b48d948049bc46de1
Zaphod <mistrz7losos@live.com>
2021-03-01 18:30:58 +0100

    Configured JMS

--- src/main/java/com/myszor/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/com/myszor/beer/inventory/service/domain/BeerInventory.java
@@ -20,7 +20,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.UUIDCharType;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -43,7 +46,10 @@ public class BeerInventory extends BaseEntity{
         this.quantityOnHand = quantityOnHand;
     }
 
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID beerId;
+
     private String upc;
     private Integer quantityOnHand = 0;
 }
